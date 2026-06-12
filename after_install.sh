46c73945720c44109de5510638ae9ac6fd38e7aa
Anthony-Akash <151724430+Anthony-Akash@users.noreply.github.com>
2026-06-12 15:59:25 +0530

    after_install

---
+++ after_install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "----------------------------------------"
+echo "Running after_install.sh script!"
+echo "----------------------------------------"
+echo "Deployment logic goes here..."
+echo "Successfully completed post-install deployment."
