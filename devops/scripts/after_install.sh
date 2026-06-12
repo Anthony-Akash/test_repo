f90e1eee5b842bad73fce899a0db4e13174b28cc
Anthony-Akash <151724430+Anthony-Akash@users.noreply.github.com>
2026-06-12 16:28:23 +0530

    test fast run 007

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -3,4 +3,7 @@ echo "----------------------------------------"
 echo "Running after_install.sh script!"
 echo "----------------------------------------"
 echo "Deployment logic goes here..."
+
+QUICK_DEPLOYMENT=FASTER_DEPLOY_PLACEHOLDER
+
 echo "Successfully completed post-install deployment."
